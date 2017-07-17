5c8f89093ae89c7a6c194fe7418257a8bceee64c
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-07-16 23:51:39 -0500

    Initial commit.

---
+++ android/src/org/yagamipaul/games/tetris/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.yagamipaul.games.tetris;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.yagamipaul.games.tetris.PaulTetrisGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PaulTetrisGame(), config);
+	}
+}
---
+++ desktop/src/org/yagamipaul/games/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.yagamipaul.games.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.yagamipaul.games.tetris.PaulTetrisGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PaulTetrisGame(), config);
+	}
+}
